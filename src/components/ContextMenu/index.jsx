bb90704ffb1fae4df3d9349c8a227c64e1491516
Ruohan <63183652+airol123@users.noreply.github.com>
2021-08-24 16:43:33 +0200

    add context menu

---
+++ src/components/ContextMenu/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import HistoryIcon from '@material-ui/icons/History';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+
+export default function SimpleList() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <List component="nav" aria-label="main mailbox folders">
+        <ListItem button>
+          <ListItemIcon>
+            <HistoryIcon />
+          </ListItemIcon>
+          <ListItemText primary="History" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <DeleteForeverIcon />
+          </ListItemIcon>
+          <ListItemText primary="Delect" />
+        </ListItem>
+      </List>
+
+    </div>
+  );
+}
